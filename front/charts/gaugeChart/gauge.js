d95c803195dad70c050b6dc428e8f3231a8323d3
unknown <>
2024-06-19 08:48:05 -0300

    remove comments

--- front/charts/gaugeChart/gauge.js
+++ front/charts/gaugeChart/gauge.js
@@ -47,21 +47,6 @@ function renderGauge() {
 
   chart.draw(data, options);
 
-  // If you want to update the chart at intervals, you can set the intervals here
-  // setInterval(function () {
-  //   fetch('http://127.0.0.1:5000/monitoramento/graficomedidor')
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       gaugeData = [
-  //         ['Label', 'Value'],
-  //         ['Luminosidade', data.MED_LUMINOSIDADE],
-  //         ['Temperatura', data.MED_TEMPERATURA],
-  //         ['Umidade', data.MED_UMIDADE],
-  //       ];
-  //       chart.draw(data, options);
-  //     })
-  //     .catch((error) => console.error('Erro ao obter dados:', error));
-  // }, 10000);
 }
 
 document.addEventListener('DOMContentLoaded', getData);
